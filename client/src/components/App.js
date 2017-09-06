1d23ef9a66705b10aae3f7139c498d0233b11070
Konstantin M <ozariss15@gmail.com>
2017-09-06 16:08:25 +0300

    Added Home and MovieDetail placeholders

---
+++ client/src/components/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Home';
+import MovieDetail from './MovieDetail';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+      	<BrowserRouter>
+      		<div>
+      			<Route path="/" component={Home} />
+      			<Route path="/movies/:id" component={MovieDetail} />
+      		</div>
+      	</BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
