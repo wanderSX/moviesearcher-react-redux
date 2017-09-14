5911682e245dd54cc764d169f5b1bf6fc9de5975
Konstantin M <ozariss15@gmail.com>
2017-09-14 06:28:23 +0300

    config tweaks

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
-	twitterConsumerKey: '4yyJ4Sk4ue1mOXvGdgyCi7FKc',
-	twitterConsumerSecret: 'c8v9L7AuKRt1oYtEzxPS6h2A23UzyepmUR8V3yA8cujxJmk4UJ',
-	cookieKey:'a9wt2hbiom7293mryzdhggh8375hgs29q',
-	mongoURI: 'mongodb://creets:password@ds127864.mlab.com:27864/movie-searcher'
+	twitterConsumerKey: <<YOUR_TWITTER_KEY>>,
+	twitterConsumerSecret: <<YOUR_TWITTER_SECRET>>,
+	cookieKey:<<YOUR_COOKIE_KEY>>,
+	mongoURI: <<YOUR_MONGO_URI>>
 }
